a804b1859236fe069cee5edd3dde7e41ee62bad2
Abhishek Nambiar <abhisheknambiar98@dev.abhishek>
2018-11-21 07:39:26 +0530

    dijkstra changed

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -1,58 +1,87 @@
 #include <stdio.h> 
 #include <limits.h> 
-   
+  
 #define N 9 
-
-int minimumDistance(int dist[], bool sptSet[]) 
+  
+int minDist(int dist[],   bool visited[]) 
 { 
-   int min = INT_MAX, min_index;  
-   for (int v = 0; v < N; v++) 
-     if (sptSet[v] == false && dist[v] <= min) 
-         min = dist[v], min_index = v; 
-   return min_index; 
+      
+    int min = INT_MAX, min_ind; 
+  
+    for (int v = 0; v < N; v++) 
+        if (visited[v] == false && 
+                   dist[v] <= min) 
+            min = dist[v], min_ind = v; 
+  
+    return min_ind; 
 } 
-   
-int display(int dist[], int n) 
+  
+void displayPath(int path[], int j) 
 { 
-   printf("Vertex   Distance from root\n"); 
-   for (int i = 0; i < N; i++) 
-      printf("%d \t %d\n", i, dist[i]); 
+    if (path[j] == - 1) 
+        return; 
+    displayPath(path, path[j]); 
+    printf("%d ", j); 
 } 
+  
 
+int displayDistance(int dist[], int n,  
+                      int path[]) 
+{ 
+    int src = 0; 
+    printf("Vertex\t Distance\tPath"); 
+    for (int i = 1; i < N; i++) 
+    { 
+        printf("\n%d -> %d \t\t %d\t\t%d ", 
+                      src, i, dist[i], src); 
+        displayPath(path, i); 
+    } 
+} 
+  
 void dijkstra(int graph[N][N], int src) 
 { 
-     int dist[N];  
-     bool sptSet[N];
- 
-     for (int i = 0; i < N; i++) 
-        dist[i] = INT_MAX, sptSet[i] = false; 
+    int dist[N];  
+    bool visited[N]; 
+  
+    int path[N]; 
 
-     dist[src] = 0; 
-     for (int i = 0;i < N-1; i++) 
-     { 
-       int u = minimumDistance(dist, sptSet); 
-       sptSet[u] = true; 
-       for (int v = 0; v < N; v++) 
-         if (!sptSet[v] && graph[u][v] && dist[u] != INT_MAX  
-                                       && dist[u]+graph[u][v] < dist[v]) 
-            dist[v] = dist[u] + graph[u][v]; 
-     } 
-     display(dist, N); 
+    for (int i = 0; i < N; i++) 
+    { 
+        path[0] = -1; 
+        dist[i] = INT_MAX; 
+        visited[i] = false; 
+    } 
+    dist[src] = 0; 
+
+    for (int i = 0;i < N - 1; i++) 
+    { 
+
+        int u = minDist(dist, visited); 
+        visited[u] = true; 
+        for (int v = 0; v < N; v++) 
+            if (!visited[v] && graph[u][v] && dist[u] + graph[u][v] < dist[v]) 
+            { 
+                path[v] = u; 
+                dist[v] = dist[u] + graph[u][v]; 
+            }  
+    } 
+  
+    displayDistance(dist, N, path); 
 } 
-   
-void main() 
-{ 
-   int graph[N][N] = {{0, 4, 0, 0, 0, 0, 0, 8, 0}, 
-                      {4, 0, 8, 0, 0, 0, 0, 11, 0}, 
-                      {0, 8, 0, 7, 0, 4, 0, 0, 2}, 
-                      {0, 0, 7, 0, 9, 14, 0, 0, 0}, 
-                      {0, 0, 0, 9, 0, 10, 0, 0, 0}, 
-                      {0, 0, 4, 14, 10, 0, 2, 0, 0}, 
-                      {0, 0, 0, 0, 0, 2, 0, 1, 6}, 
-                      {8, 11, 0, 0, 0, 0, 1, 0, 7}, 
-                      {0, 0, 2, 0, 0, 0, 6, 7, 0} 
-                     }; 
-   
+  
+int  main() 
+{  
+    int graph[N][N] = {{0, 4, 0, 0, 0, 0, 0, 8, 0}, 
+                       {4, 0, 8, 0, 0, 0, 0, 11, 0}, 
+                        {0, 8, 0, 7, 0, 4, 0, 0, 2}, 
+                        {0, 0, 7, 0, 9, 14, 0, 0, 0}, 
+                        {0, 0, 0, 9, 0, 10, 0, 0, 0}, 
+                        {0, 0, 4, 0, 10, 0, 2, 0, 0}, 
+                        {0, 0, 0, 14, 0, 2, 0, 1, 6}, 
+                        {8, 11, 0, 0, 0, 0, 1, 0, 7}, 
+                        {0, 0, 2, 0, 0, 0, 6, 7, 0} 
+                    }; 
+  
     dijkstra(graph, 0); 
-   
+    return 0;
 } 
